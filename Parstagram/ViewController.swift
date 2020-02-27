7ff96593b3810c86fd2d2a6f359b989e2c97e016
Eric <ericcai@hotmail.com>
2020-02-26 20:19:37 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Eric Cai on 2/26/20.
+//  Copyright © 2020 Eric Cai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
